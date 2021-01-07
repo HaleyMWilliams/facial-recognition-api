50758da3f732eb8587f250e5cb29429160af1b03
HaleyMWilliams <66376569+HaleyMWilliams@users.noreply.github.com>
2021-01-07 16:04:09 -0500

    heroku

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '03ecc1108a3442959b1c045faf44c9a8'
+});
+
+
+const handleApiCall = (req, res) => {
+app.models
+.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+.then(data => {
+  res.json(data);
+})
+.catch(err => res.status(400).json('unable to work with the api'))
+} 
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
--- server.js
+++ server.js
@@ -34,6 +34,8 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-app.listen(3005, () => {
-    console.log('app is running');
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(process.env.PORT || 3005,  () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
