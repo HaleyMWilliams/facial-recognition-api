c09c5aa4e23693d1e61d182d8816b30777a8cef2
HaleyMWilliams <66376569+HaleyMWilliams@users.noreply.github.com>
2021-01-08 12:22:37 -0500

    changed-default-port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
@@ -34,6 +35,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3005,  () => {
+app.listen(process.env.PORT || 3333,  () => {
     console.log(`app is running on port ${process.env.PORT}`);
 })
